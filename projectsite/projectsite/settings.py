51f7d5c7bedd3acdf6de0c9ae9539f810fdba319
crishelpc <202280108@psu.palawan.edu.ph>
2023-12-01 12:58:34 +0800

    debug true

--- projectsite/projectsite/settings.py
+++ projectsite/projectsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v33)#z4gbr+9(1@k#x==#ykm-a^*n+an^))32wd(dm!918^q9w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
